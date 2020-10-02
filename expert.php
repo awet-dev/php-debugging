4248f7f7ac931cbd96daecc6ef319ea7d178caba
Awet Tesfay <whitezscope@gmail.com>
2020-10-02 10:05:08 +0200

    array starts with 0 index,

--- expert.php
+++ expert.php
@@ -2,7 +2,6 @@
 declare(strict_types=1);
 
 // === Exercise 1 ===
-
 echo "Exercise 1 starts here:";
 // error: $x was undefined
 // solution: put the $x as parameter
@@ -11,6 +10,14 @@ function new_exercise($x) {
     echo $block;
 }
 new_exercise(2);
+
+// === Exercise 2 ===
+// expected: monday // but array in php start with 0 index, so the first element is with index of 0;
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[0];
+echo $monday;
+
+new_exercise(3);
 
 
 
