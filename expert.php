2e05884c398509fc59af02c861db2c195af3ff6d
Awet Tesfay <whitezscope@gmail.com>
2020-10-02 13:58:44 +0200

    modify the way to check email and passward to be correct

--- expert.php
+++ expert.php
@@ -97,3 +97,22 @@ function copyright(int $year) {
 //print the copyright
 echo copyright(date('Y')+0);
 
+new_exercise(8);
+// change the logical operator to && and return full name if condition fulfilled else return No access
+function login(string $email, string $password) {
+    if($email == 'john@example.be' && $password == 'pocahontas') {
+        return 'Welcome John Smith <br>';
+    } else {
+        return 'No access <br>';
+    }
+}
+
+//do not change anything below
+//should great the user with his full name (John Smith)
+echo login('john@example.be', 'pocahontas');
+//no access
+echo login('john@example.be', 'dfgidfgdfg');
+//no access
+echo login('wrong@example.be', 'wrong');
+//you can change things again!
+
