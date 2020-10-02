a41c2a703620b4eb4ba5373f56c36c1457d18c0f
Awet Tesfay <whitezscope@gmail.com>
2020-10-02 09:59:52 +0200

    the function was called. only the variable was not defined

--- expert.php
+++ expert.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 // === Exercise 1 ===
 
 echo "Exercise 1 starts here:";
-// error: $x was undefined and new_exercise function was not used/or called
-// solution: put the $x as parameter, and call the function with argument;
+// error: $x was undefined
+// solution: put the $x as parameter
 function new_exercise($x) {
     $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
     echo $block;
 }
-new_exercise(5);
+new_exercise(2);
+
+
+
 
 
