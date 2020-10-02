6d81c14488fde376439b302bb68711c5e0cc3295
Awet Tesfay <whitezscope@gmail.com>
2020-10-02 10:24:50 +0200

    change to the single quotes to the double quotes

--- expert.php
+++ expert.php
@@ -1,6 +1,10 @@
 <?php
 declare(strict_types=1);
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 // === Exercise 1 ===
 echo "Exercise 1 starts here:";
 // error: $x was undefined
@@ -18,6 +22,15 @@ $monday = $week[0];
 echo $monday;
 
 new_exercise(3);
+
+
+// === Exercise 3 ===
+// error: string is double quoted,
+// solution: if there is no variable among or inside the string make it single quotes;
+$str = 'Debugged ! Also very fun';
+echo substr($str, 0, 10);
+
+new_exercise(4);
 
 
 
