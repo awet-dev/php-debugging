3d68637e8c23d762c57de6e6bf855316f749a3e9
Awet Tesfay <whitezscope@gmail.com>
2020-10-02 09:55:51 +0200

    put the  variable ad paramenter and call the function with argument

--- expert.php
+++ expert.php
@@ -1,6 +1,15 @@
 <?php
 declare(strict_types=1);
 
+// === Exercise 1 ===
 
+echo "Exercise 1 starts here:";
+// error: $x was undefined and new_exercise function was not used/or called
+// solution: put the $x as parameter, and call the function with argument;
+function new_exercise($x) {
+    $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
+    echo $block;
+}
+new_exercise(5);
 
 
